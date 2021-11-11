d71ac72caef85a66d74a9173dd6cd148df690f61
Erick Silva <erickrsilva@hotmail.com>
2021-11-11 10:07:53 -0300

    HomePage route created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import './assets/styles/reset.css';
+import './assets/styles/style.css';
+import HomePage from "./components/pages/home/HomePage";
+import { Routes, Route } from "react-router-dom";
+
 export default function App(){
     return(
-        <div>APP</div>
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+        </Routes>
     );
 }
---
+++ src/components/shared/sharedStyles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const ContainerStyle = styled.main`
+    display: block;
+    width: 935px;
+    margin: 0 auto;
+    position: relative;
+`;
+
+export {
+    ContainerStyle
+}
