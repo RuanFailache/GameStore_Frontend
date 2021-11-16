d7c4ce73e12fb839cef1fcd00a2bc50763ff4269
Erick Silva <erickrsilva@hotmail.com>
2021-11-16 13:43:13 -0300

    Footer created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUpPage from './components/pages/login/SignUpPage';
 import { Routes, Route } from "react-router-dom";
 import CartContext from './components/contexts/CartContext';
 import { useState } from 'react';
+import Footer from './components/shared/Footer';
 
 export default function App() {
   const [productsInCart, setProductsInCart] = useState([]);
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
+      <Footer/>
     </CartContext.Provider>
   );
 }
