18c690f75e4515109076b0add5450514620fd6be
Adriano Gotardo <adrianogotardo.12@gmail.com>
2021-11-16 14:21:25 -0300

    Added the correct backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:4000';
+const URL = 'https://gamestore-back.herokuapp.com';
 
 function getProducts() {
   const promise = axios.get(`${URL}/products`);
